89143f43229857d1e8391a1c454b798fd39fb1ff
Artur Massaro <arturmassaro3@hotmail.com>
2019-08-20 13:02:46 -0300

    add put, delete & create tasks on projects

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
-
+var reqCont = 0;
 
 function hasProject(req, res, next){
   const { project } = req.body;
@@ -17,6 +17,39 @@ function hasProject(req, res, next){
   return next();
 }
 
+function hasProjectId(req,res,next){
+  const { id } = req.params;
+  const project = projects.find( project => project['id'] === id );
+
+
+  if(!project){
+    return res.status(400).json({ erro: "Project is required" })
+  }
+
+  req.project = project
+  req.projectId = id
+  return next();
+
+}
+
+function hasTitle(req,res,next){
+  const { title } = req.body;
+  if(!title){
+    return res.status(400).json({ error: 'Null title' })
+  }
+
+  req.title = title;
+
+  return next();
+}
+
+server.use((req,res,next) => {
+  reqCont++;
+
+  console.log(reqCont);
+  return next();
+});
+
 server.post('/projects', hasProject, (req, res) => {
   projects.push(req.project)
   return res.json(projects);
@@ -27,13 +60,37 @@ server.get('/projects', (req, res) => {
   
 })
 
-server.put('/projects', hasProject, (req, res) => {
-  // Aqui deve ser criado a modificaçao de um projeto 
+server.put('/projects/:id', hasProjectId, hasProject, (req, res) => {
 
-  // Se atentar as middlewares propostas no desafio !
+  const index = projects.findIndex( (project,i) => {
+    return project.id === req.projectId;
+  } );
 
-  
+  projects[index] = req.project;
+
+  return res.json(projects);
 })
 
+server.delete('/projects/:id', hasProjectId, (req,res) => {
+  const index = projects.findIndex( (project,i) => {
+    return project.id === req.projectId;
+  } );
+
+  projects.splice(index, 1);
+
+  return res.send('Project has been deleted')
+})
+
+server.post('/projects/:id/tasks', hasTitle, hasProjectId, (req,res)=> {
+  const index = projects.findIndex( (project,i) => {
+    return project.id === req.projectId;
+  } );
+  const project = req.project
+  project.tasks.push(req.title)
+
+  projects[index] = project
+
+  return res.json(projects)
+})
 
 server.listen(3000);
